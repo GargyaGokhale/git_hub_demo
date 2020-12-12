bd78eafa8f30332b2c1d20488456769880cc8bc8
Gargya Gokhale <gargya.1996@gmail.com>
2020-12-12 23:53:43 +0100

    Add input feature

--- abcd.py
+++ abcd.py
@@ -1,7 +1,11 @@
-# Python file
+"""File to print a random integer"""
+# Import packages
 import numpy as np
 
 if __name__ == '__main__':
-
-  a = np.random.randint(0, 10)
-  print(f"Number is {a}")
+    high_num = int(input("Enter upper limit of number"))
+    if high_num > 0:
+        a = np.random.randint(0, high_num)
+        print(f"Number is {a}")
+    else:
+        raise ValueError("Upper limit should be greater than 0")
